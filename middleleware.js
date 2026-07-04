13ed95d91a79b136a8ec2ac8ad593305cbf92fd2
ulokojude <myadonaitech@gmail.com>
2026-07-04 22:23:12 +0100

    Revert "removed coments from scripts"
    This reverts commit 32750255fa80c655b8b4cfdfdd88d090c21ed4d1.

--- middleleware.js
+++ middleleware.js
@@ -2,13 +2,14 @@ import { NextResponse } from 'next/server';
 import { Ratelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis';
 
+// Connect 2 d Upstash Redis db usn e. v.
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, '10 s'),
+  limiter: Ratelimit.slidingWindow(5, '10 s'), // Max 5 requests every 10 seconds
 });
 
 export async function middleware(request) {
-  
+  // Only target our logging API endpoint
   if (request.nextUrl.pathname === '/api/log') {
     const ip = request.headers.get('x-forwarded-for') || '127.0.0.1';
     const { success } = await ratelimit.limit(ip);
